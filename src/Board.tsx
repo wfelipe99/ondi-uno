4cc182030279a4235a7da94739760d981771098b
wfelipe99 <wevellysantos@gmail.com>
2022-04-17 22:14:57 -0300

    Refactored discard animation. Now works for every screen and no hacking

--- src/Board.tsx
+++ src/Board.tsx
@@ -1,50 +1,10 @@
 // @ts-nocheck
+// TODO: remove @ts-nocheck
 import type { BoardProps } from "boardgame.io/react";
 import type { OndiUnoState } from "./Game";
-import { Grid, Image, VStack, HStack, GridItem } from "@chakra-ui/react";
+import { Grid, Image, VStack, HStack, GridItem, Box } from "@chakra-ui/react";
 import MotionImage from "./MotionImage";
 import { useRef, useState } from "react";
-
-const discardCardAnimation = {
-  discard: (i) => {
-    // Número randômico entre 1 e 60
-    const number = Math.floor(Math.random() * 45) + 1;
-    const positiveOrNegative = Math.random() < 0.5 ? 1 : -1;
-
-    const degree = number * positiveOrNegative;
-
-    // TODO: tentar deixar a carta centralizada na tela sem uso de condicional
-    const baseX = () => {
-      if (window.innerWidth >= 1920) return 453;
-      else if (window.innerWidth >= 1300) return 410;
-      else if (window.innerWidth >= 1000) return 380;
-    };
-
-    const baseY = () => {
-      if (window.innerHeight >= 900) return -373;
-      else if (window.innerHeight >= 800) return -330;
-      else if (window.innerHeight <= 600) return -160;
-    };
-
-    /**
-     * i * 100 => Eu acho que o 100 está ligado ao tamanho da carta e ao spacing
-     * Quanto maior o i (index), mais para a direita está a carta. Assim, quanto mais para
-     * a direita, mais eu preciso retirar da coordenada x final desta carta
-     *
-     * */
-    return {
-      x: baseX() - i * 100,
-      y: baseY(),
-      // Rotate randomly beetwen -45° and 45°
-      rotate: degree,
-      transition: { duration: 0.6 },
-    };
-  },
-  hover: {
-    y: -50,
-    transition: { duration: 0.25 },
-  },
-};
 
 export default function OndiUnoBoard({
   ctx,
@@ -53,8 +13,42 @@ export default function OndiUnoBoard({
 }: BoardProps<OndiUnoState>) {
   const [discardThisCard, setDiscardThisCard] = useState("");
   const [discardedCards, setDiscardedCards] = useState([]);
+  const discardPlaceRef = useRef(null);
+  const cardsRef = useRef([]);
+
+  const cardAnimations = (cardId: number) => {
+    const upLengthOnHover = 50;
+
+    return {
+      discard: (i) => {
+        const number = Math.floor(Math.random() * 45) + 1;
+        const positiveOrNegative = Math.random() < 0.5 ? 1 : -1;
+
+        const degree = number * positiveOrNegative;
+
+        const discardPlaceOffsets =
+          discardPlaceRef.current.getBoundingClientRect();
+        const discardedCardOffsets =
+          cardsRef.current[cardId].getBoundingClientRect();
+
+        return {
+          x: discardPlaceOffsets.x - discardedCardOffsets.x,
+          y:
+            -(discardedCardOffsets.y - discardPlaceOffsets.y) - upLengthOnHover,
+          rotate: degree,
+          transition: { duration: 0.6 },
+        };
+      },
+      hover: {
+        y: -upLengthOnHover,
+        transition: { duration: 0.25 },
+      },
+    };
+  };
 
   const discardCard = (e) => {
+    if (discardedCards.includes(e.target.id)) return false;
+
     e.target.style.zIndex = discardedCards.length;
     setDiscardThisCard(e.target.id);
     setDiscardedCards((oldArray) => [...oldArray, e.target.id]);
@@ -69,26 +63,9 @@ export default function OndiUnoBoard({
       else ImagePath = `cards/wild_${card.wild}.png`;
 
       return elements.push(
-        // Height must be: height of card + height of animation on hover (182 + 100 = 282px)
-        /*<MotionFlex
-          id={`wrapper-card-${card.color ?? card.wild}-${
-            card.number ?? "wild"
-          }-${i}`}
-          variants={discardCardAnimation}
-          animate={
-            discardThisCard ===
-              `wrapper-card-${card.color ?? card.wild}-${
-                card.number ?? "wild"
-              }-${i}` && "discard"
-          }
-          key={i}
-          align="flex-end"
-          onClick={(e) => discardCard(e)}
-          pb="4"
-        >*/
         <MotionImage
           id={`card-${card.color ?? card.wild}-${card.number ?? "wild"}-${i}`}
-          variants={discardCardAnimation}
+          variants={cardAnimations(i)}
           custom={i}
           whileHover={
             !discardedCards.includes(
@@ -100,12 +77,13 @@ export default function OndiUnoBoard({
               `card-${card.color ?? card.wild}-${card.number ?? "wild"}-${i}` &&
             "discard"
           }
+          boxSizing="border-box"
           src={process.env.PUBLIC_URL + ImagePath}
+          ref={(el) => (cardsRef.current[i] = el)}
           onClick={(e) => discardCard(e)}
           alt="Carta"
           key={i}
         />
-        // </MotionFlex>
       );
     });
 
@@ -129,6 +107,8 @@ export default function OndiUnoBoard({
       [...Array(totalOfCards)].map((a, i) => (
         <Image
           key={i}
+          ref={(el) => (cardsRef.current[i + seat * 10] = el)}
+          id={`card-${i + seat * 10}`}
           src={process.env.PUBLIC_URL + "cards/card_back.png"}
           transform={`rotate(${transformRotateValue(seat)}deg)`}
           alt="BackCard"
@@ -161,7 +141,7 @@ export default function OndiUnoBoard({
 
       {/* Seat 0 - The player */}
       <GridItem area="5 / 1 / 6 / 7">
-        <HStack position="relative" spacing="-8" justify="center">
+        <HStack mb="4" spacing="-8" justify="center">
           {renderCardsOfThisPlayer()}
         </HStack>
       </GridItem>
@@ -180,12 +160,9 @@ export default function OndiUnoBoard({
       </GridItem>
 
       {/* Discards */}
-      {/* <GridItem placeSelf="center" area="2 / 4 / 5 / 5">
-        <Image
-          src={process.env.PUBLIC_URL + "cards/card_back.png"}
-          alt="BackCard"
-        />
-      </GridItem> */}
+      <GridItem placeSelf="center" area="2 / 4 / 5 / 5">
+        <Box minW="130" minH="182" ref={discardPlaceRef}></Box>
+      </GridItem>
     </Grid>
   );
 }
