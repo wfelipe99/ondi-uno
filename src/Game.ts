23a0fea02fb4f54c10fe6a3c5e64853f52ca3acc
wfelipe99 <wevellysantos@gmail.com>
2022-04-20 03:43:14 -0300

    Picker and pick_four done

--- src/Game.ts
+++ src/Game.ts
@@ -120,6 +120,118 @@ const uno: UnoDeck[] = [
   { id: '106', type: 'blue', number: 'picker' },
   { id: '107', type: 'pick_four', number: undefined },
   { id: '108', type: 'color_changer', number: undefined },
+
+  { id: '109', type: 'red', number: '0' },
+  { id: '110', type: 'red', number: '1' },
+  { id: '111', type: 'red', number: '2' },
+  { id: '112', type: 'red', number: '3' },
+  { id: '113', type: 'red', number: '4' },
+  { id: '114', type: 'red', number: '5' },
+  { id: '115', type: 'red', number: '6' },
+  { id: '116', type: 'red', number: '7' },
+  { id: '117', type: 'red', number: '8' },
+  { id: '118', type: 'red', number: '9' },
+  { id: '119', type: 'red', number: '1' },
+  { id: '120', type: 'red', number: '2' },
+  { id: '121', type: 'red', number: '3' },
+  { id: '122', type: 'red', number: '4' },
+  { id: '123', type: 'red', number: '5' },
+  { id: '124', type: 'red', number: '6' },
+  { id: '125', type: 'red', number: '7' },
+  { id: '126', type: 'red', number: '8' },
+  { id: '127', type: 'red', number: '9' },
+  { id: '128', type: 'red', number: 'skip' },
+  { id: '129', type: 'red', number: 'skip' },
+  { id: '130', type: 'red', number: 'reverse' },
+  { id: '131', type: 'red', number: 'reverse' },
+  { id: '132', type: 'red', number: 'picker' },
+  { id: '133', type: 'red', number: 'picker' },
+  { id: '134', type: 'pick_four', number: undefined },
+  { id: '135', type: 'color_changer', number: undefined },
+
+  { id: '136', type: 'yellow', number: '0' },
+  { id: '137', type: 'yellow', number: '1' },
+  { id: '138', type: 'yellow', number: '2' },
+  { id: '139', type: 'yellow', number: '3' },
+  { id: '140', type: 'yellow', number: '4' },
+  { id: '141', type: 'yellow', number: '5' },
+  { id: '142', type: 'yellow', number: '6' },
+  { id: '143', type: 'yellow', number: '7' },
+  { id: '144', type: 'yellow', number: '8' },
+  { id: '145', type: 'yellow', number: '9' },
+  { id: '146', type: 'yellow', number: '1' },
+  { id: '147', type: 'yellow', number: '2' },
+  { id: '148', type: 'yellow', number: '3' },
+  { id: '149', type: 'yellow', number: '4' },
+  { id: '150', type: 'yellow', number: '5' },
+  { id: '151', type: 'yellow', number: '6' },
+  { id: '152', type: 'yellow', number: '7' },
+  { id: '153', type: 'yellow', number: '8' },
+  { id: '154', type: 'yellow', number: '9' },
+  { id: '155', type: 'yellow', number: 'skip' },
+  { id: '156', type: 'yellow', number: 'skip' },
+  { id: '157', type: 'yellow', number: 'reverse' },
+  { id: '158', type: 'yellow', number: 'reverse' },
+  { id: '159', type: 'yellow', number: 'picker' },
+  { id: '160', type: 'yellow', number: 'picker' },
+  { id: '171', type: 'pick_four', number: undefined },
+  { id: '172', type: 'color_changer', number: undefined },
+
+  { id: '173', type: 'green', number: '0' },
+  { id: '174', type: 'green', number: '1' },
+  { id: '175', type: 'green', number: '2' },
+  { id: '176', type: 'green', number: '3' },
+  { id: '177', type: 'green', number: '4' },
+  { id: '178', type: 'green', number: '5' },
+  { id: '179', type: 'green', number: '6' },
+  { id: '180', type: 'green', number: '7' },
+  { id: '181', type: 'green', number: '8' },
+  { id: '182', type: 'green', number: '9' },
+  { id: '183', type: 'green', number: '1' },
+  { id: '184', type: 'green', number: '2' },
+  { id: '185', type: 'green', number: '3' },
+  { id: '186', type: 'green', number: '4' },
+  { id: '187', type: 'green', number: '5' },
+  { id: '188', type: 'green', number: '6' },
+  { id: '189', type: 'green', number: '7' },
+  { id: '190', type: 'green', number: '8' },
+  { id: '191', type: 'green', number: '9' },
+  { id: '192', type: 'green', number: 'skip' },
+  { id: '193', type: 'green', number: 'skip' },
+  { id: '194', type: 'green', number: 'reverse' },
+  { id: '195', type: 'green', number: 'reverse' },
+  { id: '196', type: 'green', number: 'picker' },
+  { id: '197', type: 'green', number: 'picker' },
+  { id: '198', type: 'pick_four', number: undefined },
+  { id: '199', type: 'color_changer', number: undefined },
+
+  { id: '200', type: 'blue', number: '0' },
+  { id: '201', type: 'blue', number: '1' },
+  { id: '202', type: 'blue', number: '2' },
+  { id: '203', type: 'blue', number: '3' },
+  { id: '204', type: 'blue', number: '4' },
+  { id: '205', type: 'blue', number: '5' },
+  { id: '206', type: 'blue', number: '6' },
+  { id: '207', type: 'blue', number: '7' },
+  { id: '208', type: 'blue', number: '8' },
+  { id: '209', type: 'blue', number: '9' },
+  { id: '210', type: 'blue', number: '1' },
+  { id: '211', type: 'blue', number: '2' },
+  { id: '212', type: 'blue', number: '3' },
+  { id: '213', type: 'blue', number: '4' },
+  { id: '214', type: 'blue', number: '5' },
+  { id: '215', type: 'blue', number: '6' },
+  { id: '216', type: 'blue', number: '7' },
+  { id: '217', type: 'blue', number: '8' },
+  { id: '218', type: 'blue', number: '9' },
+  { id: '219', type: 'blue', number: 'skip' },
+  { id: '220', type: 'blue', number: 'skip' },
+  { id: '221', type: 'blue', number: 'reverse' },
+  { id: '222', type: 'blue', number: 'reverse' },
+  { id: '223', type: 'blue', number: 'picker' },
+  { id: '224', type: 'blue', number: 'picker' },
+  { id: '225', type: 'pick_four', number: undefined },
+  { id: '226', type: 'color_changer', number: undefined },
 ]
 
 type SpecialColors = 'blue' | 'green' | 'red' | 'yellow' | null
@@ -151,6 +263,7 @@ export interface OndiUnoState {
     cards: UnoDeck[]
   }
   nextSpecialColor: SpecialColors
+  cardAccumulatorForNextPlayer: number
 }
 
 const drawCard: Move<OndiUnoState> = (G, ctx) => {
@@ -166,6 +279,11 @@ const drawCard: Move<OndiUnoState> = (G, ctx) => {
   const validCardsToDiscard = G.players[ctx.currentPlayer].filter((card) => {
     const lastDiscardedCard = G.discardedCards.cards.at(-1)
 
+    if (G.cardAccumulatorForNextPlayer > 0) {
+      if (lastDiscardedCard?.type === 'pick_four' && card.type !== 'pick_four') return false
+      if (lastDiscardedCard?.number === 'picker' && card.number !== 'picker') return false
+    }
+
     if (card.type === 'pick_four' || card.type === 'color_changer') return true
     if (card.type === lastDiscardedCard?.type || card.number === lastDiscardedCard?.number)
       return true
@@ -180,6 +298,37 @@ const drawCard: Move<OndiUnoState> = (G, ctx) => {
   }
 
   ctx.events?.endTurn()
+}
+
+const drawCards: Move<OndiUnoState> = (G, ctx, numberOfCards: number) => {
+  // @ts-ignore
+  const newAvailableCards = [].concat(G.secret.deck.availableCards)
+  const cards = newAvailableCards.splice(-numberOfCards, numberOfCards) as unknown as UnoDeck[]
+
+  G.secret.deck.total -= numberOfCards
+  G.secret.deck.availableCards = newAvailableCards
+
+  cards.map((card) => G.players[ctx.currentPlayer].push(card))
+
+  G.cardAccumulatorForNextPlayer = 0
+
+  /*const validCardsToDiscard = G.players[ctx.currentPlayer].filter((card) => {
+    const lastDiscardedCard = G.discardedCards.cards.at(-1)
+
+    if (lastDiscardedCard?.type === 'pick_four' && card.type !== 'pick_four') return false
+    if (lastDiscardedCard?.number === 'picker' && card.number !== 'picker') return false
+
+    if (card.type === 'pick_four' || card.type === 'color_changer') return true
+    if (card.type === lastDiscardedCard?.type || card.number === lastDiscardedCard?.number)
+      return true
+    if (card.type === G.nextSpecialColor) return true
+
+    return false
+  })
+
+  if (validCardsToDiscard.length !== 0) {
+    ctx.events?.setActivePlayers({ currentPlayer: 'discard' })
+  }*/
 }
 
 const discardCard: Move<OndiUnoState> = (G, ctx, card: UnoDeck) => {
@@ -206,6 +355,9 @@ const discardCard: Move<OndiUnoState> = (G, ctx, card: UnoDeck) => {
 
     return true
   })
+
+  if (card.type === 'pick_four') G.cardAccumulatorForNextPlayer += 4
+  if (card.number === 'picker') G.cardAccumulatorForNextPlayer += 2
 
   G.players[ctx.currentPlayer] = newCardsOfThisPlayer
   G.discardedCards.total++
@@ -270,6 +422,10 @@ export const OndiUno: Game<OndiUnoState> = {
     }
 
     const startCard: UnoDeck = deck.draw()
+    let cardAccumulatorForNextPlayer = 0
+
+    if (startCard.type === 'pick_four') cardAccumulatorForNextPlayer += 4
+    if (startCard.number === 'picker') cardAccumulatorForNextPlayer += 2
 
     // Precisa ser depois de distribuir as cartas
     const deckInfo: Deck = {
@@ -299,6 +455,7 @@ export const OndiUno: Game<OndiUnoState> = {
         cards: [startCard],
       },
       nextSpecialColor: null,
+      cardAccumulatorForNextPlayer,
     }
   },
 
@@ -308,6 +465,11 @@ export const OndiUno: Game<OndiUnoState> = {
     onBegin: (G, ctx) => {
       const validCardsToDiscard = G.players[ctx.currentPlayer].filter((card) => {
         const lastDiscardedCard = G.discardedCards.cards.at(-1)
+
+        if (G.cardAccumulatorForNextPlayer > 0) {
+          if (lastDiscardedCard?.type === 'pick_four' && card.type !== 'pick_four') return false
+          if (lastDiscardedCard?.number === 'picker' && card.number !== 'picker') return false
+        }
 
         if (card.type === 'pick_four' || card.type === 'color_changer') return true
         if (card.type === lastDiscardedCard?.type || card.number === lastDiscardedCard?.number)
@@ -325,6 +487,12 @@ export const OndiUno: Game<OndiUnoState> = {
         console.log(`Há cartas válidas para o player ${ctx.currentPlayer}, próxima fase discard`)
         ctx.events?.setActivePlayers({ currentPlayer: 'discard' })
         return G
+      }
+
+      if (G.cardAccumulatorForNextPlayer > 0) {
+        console.log('Puxa esse leque')
+        drawCards(G, ctx, G.cardAccumulatorForNextPlayer)
+        ctx.events?.pass()
       }
 
       console.log(`Não há cartas válidas para o player ${ctx.currentPlayer}, fase de draw`)
@@ -355,5 +523,6 @@ export const OndiUno: Game<OndiUnoState> = {
     },
     discardCard,
     chooseColor,
+    drawCards,
   },
 }
